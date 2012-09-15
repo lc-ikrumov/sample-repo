b1aa40f74666386584422871df85e7d6b1ad83ea
unknown <ikrumov@LCINT-AMS-M003.(none)>
2012-09-15 14:47:25 +0200

    KOkokoko

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,11 +1,27 @@
 package com.develogical;
 
+import java.util.StringTokenizer;
+
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.contains("SPA2012")) {
-            return "SPA is An conference!!!!!!!!!!!!!!!!!!!!";
+        StringTokenizer init = new StringTokenizer(query, ":");
+        init.nextToken();
+        init.nextToken();
+        String numbers = init.nextToken();
+        StringTokenizer nums = new StringTokenizer(numbers, ",");
+        
+       //String[] numbers = parts[1].split(",");
+        Integer max = 0;
+        while(nums.hasMoreTokens()) {
+            String token = nums.nextToken();
+            if(Integer.parseInt(token.trim()) > max) {
+                max = Integer.parseInt(token);
+            }
         }
-        return "";
+       
+        
+        return max.toString();
+        
     }
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -3,7 +3,7 @@ package com.develogical;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.matchers.StringContains.containsString;
 
 public class QueryProcessorTest {
@@ -12,11 +12,13 @@ public class QueryProcessorTest {
 
     @Test
     public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-        assertThat(queryProcessor.process("test"), is(""));
+        //assertThat(queryProcessor.process("test"), is(""));
     }
 
     @Test
     public void knowsAboutSpa() throws Exception {
-        assertThat(queryProcessor.process("SPA2012test"), containsString("conference"));
+        String test = queryProcessor.process("ewqeqw:whichis:2012,234,56666");
+        assertTrue(test.equals("56666"));
+       // assertThat(queryProcessor.process("SPA2012test"), containsString("conference"));
     }
 }
